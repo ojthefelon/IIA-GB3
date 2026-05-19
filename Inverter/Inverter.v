558c773e45ac6f5f8b0de9b27a6453bd64cc9773
Owen Marlovits <om399@cam.ac.uk>
2026-05-19 18:38:25 +0100

    upgraded Inverter branch to final version, will test tomorrow

--- Inverter/Inverter.v
+++ Inverter/Inverter.v
@@ -1,13 +1,17 @@
-module inverter (
+module top (
     input CLK,
     output LED1,
-)
+);
     reg A = 0;
     assign LED1 = A;
+    reg [23:0] clkdiv = 0;
 
     always @(posedge CLK) begin
         if (clkdiv == 1200000) begin
             A<=!A;
+            clkdiv<=0;
+        end else begin
+            clkdiv<=clkdiv+1;
         end
     end
 endmodule 
